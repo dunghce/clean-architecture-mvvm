edd3292081c1bfb48e5140b1b0b0ef7c75983afd
dunghce <dung.ntm@softworld.jp>
2023-02-06 14:46:24 +0700

    update

---
+++ clean_architecture_mvvm/lib/domain/models/post.dart
@@ -0,0 +1,18 @@
+class Poss {
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+  Poss({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+}
+//  {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//   },
---
+++ clean_architecture_mvvm/lib/presentation/post/post_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostDemoScreen extends StatefulWidget {
+  const PostDemoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PostDemoScreen> createState() => _PostDemoScreenState();
+}
+
+class _PostDemoScreenState extends State<PostDemoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
