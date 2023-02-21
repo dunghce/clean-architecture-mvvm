87b70fdb9c79d476baf10d6bc4b6c3c0bfe99f6f
dunghce <dung.ntm@softworld.jp>
2023-02-21 10:50:29 +0700

    add json_annotation

--- clean_architecture_mvvm/lib/data/reponses/post_response.dart
+++ clean_architecture_mvvm/lib/data/reponses/post_response.dart
@@ -1,11 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'post_response.g.dart';
+
+@JsonSerializable()
 class CategoryPost {
   final List<PostResponse> result;
   CategoryPost(this.result);
-  factory CategoryPost.fromJson(Map<String, dynamic> json) {
-    return CategoryPost(json['result']);
-  }
+  factory CategoryPost.fromJson(Map<String, dynamic> json) =>
+      _$CategoryPostFromJson(json);
 }
 
+@JsonSerializable()
 class PostResponse {
   final int id;
   final int userId;
@@ -17,12 +21,6 @@ class PostResponse {
     required this.title,
     required this.body,
   });
-  factory PostResponse.fromJson(Map<String, dynamic> json) {
-    return PostResponse(
-      id: json['id'] ?? 0,
-      userId: json['userId'] ?? 0,
-      title: json['title'] ?? '',
-      body: json['body'] ?? '',
-    );
-  }
+  factory PostResponse.fromJson(Map<String, dynamic> json) =>
+      _$PostResponseFromJson(json);
 }
